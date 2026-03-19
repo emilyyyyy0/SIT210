82b8e7bbc08f853c66904e9f35ede36703296828
emilyyyyy0 <emilyotoole05@gmail.com>
2026-03-19 21:08:46 +1100

    modularised by adding light function
    Refactored light control logic into a separate function for better modularity.

--- LINDAS_PORCH1.1P.ino
+++ LINDAS_PORCH1.1P.ino
@@ -1,9 +1,22 @@
 int PORCH_LIGHT = 12;
 int HALLWAY_LIGHT = 10;
 int BUTTON = 4;
-
+//NEED TO MAKE MORE MODULAR 
 unsigned long PORCH_TIME = 30000;
 unsigned long HALLWAY_TIME = 60000;
+
+void lights() //more modular??
+{
+ digitalWrite(PORCH_LIGHT, HIGH);
+    digitalWrite(HALLWAY_LIGHT, HIGH);
+
+    delay(PORCH_TIME);  // porch light duration
+    digitalWrite(PORCH_LIGHT, LOW);
+
+    delay(HALLWAY_TIME - PORCH_TIME);  // remaining hallway time
+    digitalWrite(HALLWAY_LIGHT, LOW);
+
+}
 
 void setup ()
 {
@@ -17,15 +30,8 @@ void loop()
 {
   int buttonState = digitalRead(BUTTON);
 
-  if (buttonState == LOW)
+  if (buttonState == LOW) //just need to double check they light up at the same time and not run one then the other :)
   {
-    digitalWrite(PORCH_LIGHT, HIGH);
-    digitalWrite(HALLWAY_LIGHT, HIGH);
-
-    delay(PORCH_TIME);  // porch light duration
-    digitalWrite(PORCH_LIGHT, LOW);
-
-    delay(HALLWAY_TIME - PORCH_TIME);  // remaining hallway time
-    digitalWrite(HALLWAY_LIGHT, LOW);
+    lights();
   }
 }
